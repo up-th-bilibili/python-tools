af87b32c62f5d44fe793c1e9e69a4ea2e4617086
up泰和 <1710318399@qq.com>
2026-09-24 21:10:10 +0800

    Refactor smd_to_html function for improved readability

--- SimpleMarkdown/smdc.py
+++ SimpleMarkdown/smdc.py
@@ -86,71 +86,119 @@ HTML_SPECIAL = {
 HTML_SPECIAL_TRANS = str.maketrans(HTML_SPECIAL)
 html_string = lambda string:string.translate(HTML_SPECIAL_TRANS)
 html_attribute_from_dict = lambda dct:' '.join(key if value is True else (f'{key}="{html_string(value)}"') for key,value in dct.items() if value not in (False, None))
-def smd_to_html(smd,extensions,_rec_shift=0):
+smd_ident = ('**','*','~','__','_','==','^')
+smd_ident_elem = ('strong','em','del','ins','sub','mark','sup')
+confused_ident_index = (1,-1,-1,4,-1,-1,-1)
+def html_matched(html,matched):
+    if isinstance(html,dict):
+        if html['type'] == 'html_begin':
+            html = f'<{html["name"]} {html_attribute_from_dict(html["attribute"])}>' if html['attribute'] else f'<{html["name"]}>'
+        elif html['type'] == 'html_end':
+            html = f'</{html["name"]}>'
+        elif html['type'] == 'html_self_closed':
+            html = f'<{html["name"]} {html_attribute_from_dict(html["attribute"])}/>' if html['attribute'] else f'<{html["name"]}/>'
+    if isinstance(matched,str):
+        return html == matched
+    return any(html == i for i in matched)
+def smd_to_html(smd,extensions,_rec_shift=1,_rec_end=-1,_p_label='p',_rec_variables={}):
     ESCAPE_TABLE = {'*','\\','`','~','#','[',']','{','}','^','='}
     MAPPING_TABLE = {
         '\n':''
     }
-    i = 0
-    result = []
-    stack = []
-    expect = []
-    extra = []
-    smd = smd+'\n'
-    smd_size = len(smd)-1
-    def check_match(expect):
-        if isinstance(expect,str):
-            return len(expect)*smd.startswith(expect,i)
-        for exp in expect:
-            if exp is None:
-                continue
-            if smd.startswith(exp,i):
-                return len(exp)
-        return False
-    def important_hint_decl(expect,stack,extra,result,cond):
-        if cond == 2:
-            if stack and stack[-1]['type'] == 'html_end' and stack[-1]['name'] == 'strong':
-                nonlocal i
-                i -= 1
-                return
-            result.pop()
-            expect.append(['**','*'])
-            stack.append({'type':'html_end','name':'em'})
-            extra.append(important_hint_decl)
-            expect.append('**')
-            result.append({'type':'html_begin','name':'strong','attribute':{}})
-            stack.append({'type':'html_end','name':'strong'})
-            extra.append(None)
+    i = _rec_shift
+    result = _rec_variables.get('result',[])
+    stack = _rec_variables.get('stack',[])
+    expect = _rec_variables.get('expect',[])
+    ident = smd_ident
+    matched = _rec_variables.get('matched',[-1]*len(ident))
+    matched_end = _rec_variables.get('matched_end',[-1]*len(ident))
+    smd = '\n'*(_rec_shift==1)+smd
+    smd_size = len(smd) if _rec_end == -1 else _rec_end
+    popen = False
+    def group_matched(smd,i):
+        for j,k in enumerate(ident):
+            if smd.startswith(k,i):
+                return j
+        return -1
+    first_check_nl_over = False
     while i < smd_size:
-        cond = expect and check_match(expect[-1])
-        if cond:
-            i += cond
-            expect.pop()
-            result.append(stack.pop())
-            decl = extra.pop()
-            if decl is not None:
-                decl(expect,stack,extra,result,cond)
-        elif smd[i] == '\n':
-            next_newline = smd.find('\n',i+1)
-            if next_newline-i==4 and smd.startswith('\n---',i):
+        cond = group_matched(smd,i)
+        if cond != -1:
+            if not popen:
+                result.append({'type':'html_begin','name':_p_label,'attribute':{}})
+                popen = True
+            if confused_ident_index[cond] == -1:
+                if matched_end[cond] < matched[cond]:
+                    result.append({'type':'html_end','name':smd_ident_elem[cond]})
+                    matched_end[cond] = i
+                else:
+                    result.append({'type':'html_begin','name':smd_ident_elem[cond],'attribute':{}})
+                    matched[cond] = i
+                i+=len(ident[cond])
+            else:
+                i += len(ident[cond])
+                second_cond = group_matched(smd,i)
+                if second_cond == confused_ident_index[cond]:
+                    i += 1
+                    if matched_end[cond] < matched[cond]:
+                        if matched_end[second_cond] < matched[second_cond]:
+                            if matched[cond] <= matched[second_cond]:
+                                result.append({'type':'html_end','name':smd_ident_elem[second_cond]})
+                                result.append({'type':'html_end','name':smd_ident_elem[cond]})
+                            else:
+                                result.append({'type':'html_end','name':smd_ident_elem[cond]})
+                                result.append({'type':'html_end','name':smd_ident_elem[second_cond]})
+                            matched_end[cond] = i
+                            matched_end[second_cond] = i
+                        else:
+                            result.append({'type':'html_end','name':smd_ident_elem[cond]})
+                            result.append({'type':'html_begin','name':smd_ident_elem[second_cond],'attribute':{}})
+                            matched_end[cond] = i
+                            matched[second_cond] = i
+                    else:
+                        if matched_end[second_cond] <= matched[second_cond]:
+                            result.append({'type':'html_end','name':smd_ident_elem[second_cond]})
+                            result.append({'type':'html_begin','name':smd_ident_elem[cond],'attribute':{}})
+                            matched[cond] = i
+                            matched_end[second_cond] = i
+                        else:
+                            result.append({'type':'html_begin','name':smd_ident_elem[cond],'attribute':{}})
+                            result.append({'type':'html_begin','name':smd_ident_elem[second_cond],'attribute':{}})
+                            matched[cond] = i
+                            matched[second_cond] = i
+                else:
+                    if matched_end[cond] < matched[cond]:
+                        result.append({'type':'html_end','name':smd_ident_elem[cond]})
+                        matched_end[cond] = i
+                    else:
+                        result.append({'type':'html_begin','name':smd_ident_elem[cond],'attribute':{}})
+                        matched[cond] = i
+        elif smd[i] == '\n' or (i == _rec_shift and not first_check_nl_over):
+            next_newline = smd.find('\n',i+1,smd_size)
+            if next_newline == -1:
+                next_newline = smd_size
+            if next_newline-i>=4 and smd.startswith('---',i+(smd[i] == '\n')):
                 result.append({'type':'string','data':'\n'})
                 result.append({'type':'html_self_closed','name':'hr','attribute':{}})
                 i=next_newline
-            elif not any(not smd[i].isspace() for i in range(i+1,next_newline-1)):
-                if result and result[0] != {'type':'html_begin','name':'p','attribute':{}}:
-                    result.insert(0,{'type':'html_begin','name':'p','attribute':{}})
-                result.append({'type':'html_end','name':'p'})
-                result.append({'type':'string','data':'\n'})
-                result.append({'type':'html_begin','name':'p','attribute':{}})
+            elif not any(not smd[i].isspace() for i in range(i,next_newline-1)):
+                if popen:
+                    popen=False
+                    result.append({'type':'html_end','name':_p_label})
+                    result.append({'type':'string','data':'\n'})
                 i = next_newline
-            elif next_newline-i>=4 and smd.startswith('\n```',i):
+            elif next_newline-i>=4 and smd.startswith('```',i+(smd[i] == '\n')):
+                if popen:
+                    popen = False
+                    result.append({'type':'html_end','name':_p_label})
+                    result.append({'type':'string','data':'\n'})
                 if not result or result[-1]!={'type':'string','data':'\n'}:
                     result.append({'type':'string','data':'\n'})
                 result.append({'type':'html_begin','name':'details','attribute':{'open':True}})
                 result.append({'type':'string','data':'\n'})
                 result.append({'type':'html_begin','name':'summary','attribute':{}})
                 result.append({'type':'html_begin','name':'strong','attribute':{}})
-                result.append({'type':'string','data':smd[i+4:next_newline] or 'code'})
+                result.append({'type':'string','data':smd[i+4:next_newline] or 'plaintext'})
                 result.append({'type':'html_end','name':'strong'})
                 result.append({'type':'html_end','name':'summary'})
                 result.append({'type':'string','data':'\n'})
@@ -172,44 +220,68 @@ def smd_to_html(smd,extensions,_rec_shift=0):
                 result.append({'type':'string','data':'\n'})
                 result.append({'type':'html_end','name':'details'})
                 i = next_newline
-            else:
-                if not(result and((result[-1]['type'] == 'html_end' and result[-1]['name'] == 'code')or(result[-1]['type'] == 'html_self_closed' and result[-1]['name'] == 'hr'))):
+            elif next_newline-i>1 and smd.startswith('#',i+(smd[i] == '\n')):
+                if popen:
+                    popen = False
+                    result.append({'type':'html_end','name':_p_label})
+                    result.append({'type':'string','data':'\n'})
+                i += (smd[i] == '\n')
+                start = i
+                while next_newline < smd_size and i < next_newline and smd[i] == '#':
+                    i += 1
+                head_len = i - start
+                if head_len > 6:
+                    sys.stderr.write(f'Warning: expect # ~ ###### and get {"#"*head_len}.')
+                    head_len = 6
+                if i >= next_newline:
+                    sys.stderr.write(f'Warning: empty head.')
+                    result.append({'type':'html_begin','name':f'h{head_len}','attribute':{}})
+                    result.append({'type':'html_end','name':f'h{head_len}'})
+                    i = next_newline
+                    continue
+                if smd[i] in '\t\f\r ':
+                    i += 1
+                rec_var = {}
+                smd_to_html(smd,extensions,i,next_newline-(smd[next_newline-1] == '\\'),f'h{head_len}',rec_var)
+                while smd[next_newline-1] == '\\':
+                    i = next_newline
+                    if(i==smd_size):
+                        break
+                    next_newline = smd.find('\n',i+1,smd_size)
+                    if next_newline == -1:
+                        next_newline = smd_size
+                    for count in range(head_len):
+                        if i < next_newline and smd[i] in '\t\f\r ':
+                            i += 1
+                        else:
+                            break
+                    smd_to_html(smd,extensions,i,next_newline-(smd[next_newline-1] == '\\'),f'h{head_len}',rec_var)
+                result.extend(rec_var['result'])
+                result.append({'type':'html_end','name':f'h{head_len}'})
+                i = next_newline
+            elif smd[i]=='\n':
+                if not(result[:-1] and(html_matched(result[-2],('<hr/>','</details>','</ul>','</ol>','<p>','</h1>','</h2>','</h3>','</h4>','</h5>','</h6>','</pre>')))):
                     result.append({'type':'html_self_closed','name':'br','attribute':{}})
                 i += 1
-            result.append({'type':'string','data':'\n'})
-        elif smd.startswith('**',i):
-            expect.append('**')
-            result.append({'type':'html_begin','name':'strong','attribute':{}})
-            stack.append({'type':'html_end','name':'strong'})
-            extra.append(None)
-            i+=2
-        elif smd[i] == '*':
-            expect.append(['**','*'])
-            result.append({'type':'html_begin','name':'em','attribute':{}})
-            stack.append({'type':'html_end','name':'em'})
-            extra.append(important_hint_decl)
-            i+=1
+            else:
+                first_check_nl_over = True
+                continue
+            if (not result)or (result[-1]!={'type':'string','data':'\n'}):
+                result.append({'type':'string','data':'\n'})
         elif smd[i] == '`':
             result.append({'type':'html_begin','name':'code','attribute':{}})
+            start = i+1
             while smd[i] != '`' and smd[i] != '\n':
                 i += 1
+            result.append({'type':'string_range','range':[start,i]})
             if smd[i] != '`':
                 sys.stderr.write('unclosed single-line code.')
             result.append({'type':'html_end','name':'code'})
             i += 1
-        elif smd.startswith('==',i):
-            expect.append('==')
-            result.append({'type':'html_begin','name':'mark','attribute':{}})
-            stack.append({'type':'html_end','name':'mark'})
-            extra.append(None)
-            i+=2
-        elif smd[i] == '~':
-            expect.append('~')
-            result.append({'type':'html_begin','name':'del','attribute':{}})
-            stack.append({'type':'html_end','name':'del'})
-            extra.append(None)
-            i+=1
         else:
+            if not popen:
+                result.append({'type':'html_begin','name':_p_label,'attribute':{}})
+                popen = True
             if smd[i] == '\\':
                 i += 1
                 if smd[i] in ESCAPE_TABLE:
@@ -222,6 +294,9 @@ def smd_to_html(smd,extensions,_rec_shift=0):
                     data = smd[i+1:i+5]
                     if any(i not in '0123456789abcdefABCDEF' for i in data):
                         sys.stderr.write(f'Error: wrong unicode hex "{data}".')
+                        result.append({'type':'html_begin','name':'strong','attribute':{'style':'color:#ff0000;'}})
+                        result.append({'type':'string','data':f'U+{data}'})
+                        result.append({'type':'html_end','name':'strong'})
                     else:
                         data = eval(f'"\\u{data}"')
                         result.append({'type':'string','data':data})
@@ -234,8 +309,11 @@ def smd_to_html(smd,extensions,_rec_shift=0):
             else:
                 result.append({'type':'string_range','range':[i,i+1]})
             i += 1 #TODO:实现该分支
-    if result and result[0] == {'type':'html_begin','name':'p','attribute':{}}:
-        result.append({'type':'html_end','name':'p'})
+    if _rec_shift-1:
+        _rec_variables.update(locals())
+        return result
+    if popen:
+        result.append({'type':'html_end','name':_p_label})
     for i,j in enumerate(result):
         if j['type'] == 'string':
             result[i] = html_string(j['data'])
